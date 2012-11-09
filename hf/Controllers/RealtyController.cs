57f105a11a09748491931f13bfadd1bd1b9c1afa
matthew <matthun@gmail.com>
2012-11-09 03:03:53 +0100

    Init.

---
+++ hf/Controllers/RealtyController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using Database.Models;
+
+namespace Database.Controllers
+{
+    public class RealtyController : ApiController
+    {
+        static readonly IRealtyRepository repository = new RealtyRepository();
+
+        public IEnumerable<Realty> GetAllRealties()
+        {
+            return repository.GetAll();
+        }
+
+        public Realty GetRealty(int id)
+        {
+            Realty item = repository.Get(id);
+            if (item == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            return item;
+        }
+        
+    }
+}
