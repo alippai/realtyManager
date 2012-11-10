57d453ae69825ef0ba32a5250181e4b98ff31953
alippai <adam@rigo.sk>
2012-11-10 19:47:47 +0100

    Added dedafult DbContextInitializer.

---
+++ Database/Models/DatabaseContextInitializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Database.Models
+{
+    public class DatabaseContextInitializer : DropCreateDatabaseIfModelChanges<DatabaseContext>
+    {
+        protected override void Seed(DatabaseContext context)
+        {
+            // Use the context to seed the db.
+        }
+    }
+}
