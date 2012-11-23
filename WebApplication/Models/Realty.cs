5ffc84099ea42a31cb87585d65d6240d6c0db944
alippai <adam@rigo.sk>
2012-11-23 01:03:46 +0100

    Added custom login and DropDown for realty type.

--- WebApplication/Models/Realty.cs
+++ WebApplication/Models/Realty.cs
@@ -5,12 +5,18 @@ using System.Web;
 
 namespace RealtyManager.Models
 {
+    public enum TypeNames { 
+        Room,
+        Flat,
+        House,
+        Sublet
+    }
     public class Realty
     {
         public int RealtyId { get; set; }
         public string Address { get; set; }
         public double Size { get; set; }
-        public string Type { get; set; }
+        public TypeNames Type { get; set; }
         public double Room { get; set; }
         public decimal Price { get; set; }
         public string Description { get; set; }
